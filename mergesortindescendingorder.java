e120b23bdb52011397fabd47923e00c617c41c37
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2022-03-13 14:54:04 +0530

    11thmarch_2Q

---
+++ mergesortindescendingorder.java
@@ -0,0 +1,52 @@
+package project144;
+import java.util.Arrays;
+public class mergesortindescendingorder {
+	 public static void mergeSort(char arr[],char temp[],int low,int high){
+	        if(low<high){
+	            int mid=low+(high-low)/2;
+	            mergeSort(arr,temp,low,mid);
+	            mergeSort(arr,temp,mid+1,high);
+	            merge(arr, temp,low,mid,high);
+	        }
+	    }
+	    public static void merge(char arr[],char temp[],int low,int mid,int high){
+	        for(int i=low;i<=high;i++){
+	            temp[i]=arr[i];
+	        }
+	        int i=low;
+	        int j=mid +1;
+	        int k=low;
+	        while(i<=mid && j<=high){
+	            if(temp[i]>temp[j]){
+	                arr[k]=temp[i];
+	                i++;
+	                k++;
+	            }
+	            else{
+	                arr[k]=temp[j];
+	                j++;
+	                k++;
+	            }
+	        }
+	        while(i<=mid){
+	            arr[k]=temp[i];
+	            i++;
+	            k++;
+	        }
+	        while (j<=high){
+	            arr[k]=temp[j];
+	            j++;
+	            k++;
+	        }
+	    }
+	    public static void main(String[] args) {
+	        char  arr[] = {'y','a','b','y','A','G','t'};  //  [64,37,23,67,32,99,12,28,19,54,34,89,33,67,49]
+	        char temp[]=new char[arr.length];
+	        int low=0;
+	        int high= arr.length-1;
+
+	        mergeSort(arr,temp,low,high);
+	        System.out.println(Arrays.toString(arr));
+
+	    }
+}
